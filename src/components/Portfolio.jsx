1b1f764582cc53cc2d68a9f48bb636f211370798
Karen Luik <karen.luik5@gmail.com>
2024-11-25 15:49:47 +0100

    update cols

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,7 @@ function Portfolio() {
     <>
     <div className='mt-20 p-2 text-center'><Title>Projects</Title></div>
     <div className='flex flex-col md:flex-row items-center justify-center mb-10'>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4'>
         {portfolio.map(project=>(
           <PortfolioItem
             image={project.image}
