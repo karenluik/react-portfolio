5dd69d82b3b1af25c62d63666df9e1641caae225
Karen Luik <karen.luik5@gmail.com>
2024-11-25 15:51:46 +0100

    Update PortfolioItem.jsx

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -7,7 +7,7 @@ function PortfolioItem({title,url,image,stack}){
       <div className="w-full p-4">
         <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">{title}</h3>
         <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm mb-5">{stack.map(item => (<span className="inline-block px-2 py-1 font-semibold border-2 border-stone-000 rounded-md">{item}</span> ))}</p>
-        <a href={url}><button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Live Project</button></a>
+        <a href={url} target="_blank"><button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Live Project</button></a>
       </div>
     </div>
     
